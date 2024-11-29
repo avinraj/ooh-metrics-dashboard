07c00df4d9569a0a825cc10135181ba137bf00a1
avinraj <avinraj@nickelytics.com>
2024-11-29 12:16:22 +0530

    fix: fixed ad type issue in refresh

--- src/app/core/components/AdType.tsx
+++ src/app/core/components/AdType.tsx
@@ -9,83 +9,146 @@ import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import LocalTaxiIcon from "@mui/icons-material/LocalTaxi";
 import TvIcon from "@mui/icons-material/Tv";
 import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
-import { Accordion, AccordionDetails, AccordionSummary, Button, Divider, Typography, useTheme } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Divider,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { SET_SELECTED_ADTYPE } from "../../../store/actions";
+import StorageService from "../services/storage.serive";
 
 interface AdTypeProps {
   onOpenSwitchModal: () => void;
-  onSelectAdType: (adType: { label: string; icon: JSX.Element | null, value: string }) => void;
+  onSelectAdType: (adType: {
+    label: string;
+    icon: JSX.Element | null;
+    value: string;
+  }) => void;
 }
 
-const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) => {
+const AdType: React.FC<AdTypeProps> = ({
+  onOpenSwitchModal,
+  onSelectAdType,
+}) => {
   const theme = useTheme();
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const storageService = new StorageService();
+
   const [expanded, setExpanded] = useState<string | false>(false);
   const [expanded2, setExpanded2] = useState<string | false>(false);
   const selectedItem = useSelector((state: any) => state?.selectedAdType);
-  const [selectedAdType, setSelectedAdType] = useState<{ label: string; icon: JSX.Element | null; value: string }>(
-    selectedItem
-  );
+  const [selectedAdType, setSelectedAdType] = useState<{
+    label: string;
+    icon: JSX.Element | null;
+    value: string;
+  }>(selectedItem);
 
-  const handleAccordionChange = (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+  const handleAccordionChange =
+    (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
 
-  const handleAccordionChange2 = (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
-    setExpanded2(isExpanded ? panel : false);
-  };
+  const handleAccordionChange2 =
+    (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded2(isExpanded ? panel : false);
+    };
 
   const handleButtonClick = (label: string, icon: any, value: any) => {
-    const selectedAdType = { label, icon, value };
-    onSelectAdType(selectedAdType); // Pass the selected ad type to the parent
+    const selectedVal = { label, icon, value };
+    onSelectAdType(selectedVal); // Pass the selected ad type to the parent
     onOpenSwitchModal();
     setExpanded(false);
     setExpanded2(false);
     setSelectedAdType({ label, icon, value });
     dispatch({
       type: SET_SELECTED_ADTYPE,
-      selectedAdType: selectedAdType,
+      selectedAdType: selectedVal,
     });
+    storageService.set("local", "adType", value);
   };
 
   const adTypeConfig = [
     {
       title: t("adtype.Vehicle Ads"),
       options: [
-        { label: t("adtype.vehicles.Cars"), icon: <DirectionsCarIcon />, value: "cars" },
-        { label: t("adtype.vehicles.Buses"), icon: <DirectionsBusIcon />, value: "buses" },
-        { label: t("adtype.vehicles.Trucks"), icon: <LocalShippingIcon />, value: "trucks" },
-        { label: t("adtype.vehicles.e-scooter"), icon: <ElectricScooterIcon />, value: "escooters" },
-        { label: t("adtype.vehicles.2-wheelers"), icon: <TwoWheelerIcon />, value: "twowheelers" },
+        {
+          label: t("adtype.vehicles.Cars"),
+          icon: <DirectionsCarIcon />,
+          value: "cars",
+        },
+        {
+          label: t("adtype.vehicles.Buses"),
+          icon: <DirectionsBusIcon />,
+          value: "buses",
+        },
+        {
+          label: t("adtype.vehicles.Trucks"),
+          icon: <LocalShippingIcon />,
+          value: "trucks",
+        },
+        {
+          label: t("adtype.vehicles.e-scooter"),
+          icon: <ElectricScooterIcon />,
+          value: "escooters",
+        },
+        {
+          label: t("adtype.vehicles.2-wheelers"),
+          icon: <TwoWheelerIcon />,
+          value: "twowheelers",
+        },
       ],
     },
     {
       title: t("adtype.Digital Screens"),
       options: [
-        { label: t("adtype.digitalScreens.digitalBillboards"), icon: <TvIcon />, value: "digitalBillboards" },
-        { label: t("adtype.digitalScreens.Car Rooftoppers"), icon: <LocalTaxiIcon />, value: "carRooftoppers" },
-        { label: t("adtype.digitalScreens.Van Digital Billboards"), icon: <LocalShippingIcon />, value: "digitalBillboards" },
-        { label: t("adtype.digitalScreens.Backpacks"), icon: <HikingIcon />, value: "backpacks" },
+        {
+          label: t("adtype.digitalScreens.digitalBillboards"),
+          icon: <TvIcon />,
+          value: "digitalBillboards",
+        },
+        {
+          label: t("adtype.digitalScreens.Car Rooftoppers"),
+          icon: <LocalTaxiIcon />,
+          value: "carRooftoppers",
+        },
+        {
+          label: t("adtype.digitalScreens.Van Digital Billboards"),
+          icon: <LocalShippingIcon />,
+          value: "digitalBillboards",
+        },
+        {
+          label: t("adtype.digitalScreens.Backpacks"),
+          icon: <HikingIcon />,
+          value: "backpacks",
+        },
       ],
     },
     {
       title: t("adtype.Mobile Ads"),
       options: [],
       icon: <AdUnitsIcon />,
-      value: "mobileAds"
+      value: "mobileAds",
     },
     {
       title: t("adtype.Static Billboards"),
       options: [],
       icon: <LightbulbIcon />,
-      value: "statisBillboards"
+      value: "statisBillboards",
     },
   ];
-  const renderButton = (label: string, icon: JSX.Element | null, value: string) => {
+  const renderButton = (
+    label: string,
+    icon: JSX.Element | null,
+    value: string
+  ) => {
     const isSelected = selectedAdType?.label === label;
 
     return (
@@ -98,7 +161,9 @@ const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) =>
             textAlign: "start",
             fontSize: "13px",
             color: "black",
-            backgroundColor: isSelected ? theme.palette.primary.main : "transparent",
+            backgroundColor: isSelected
+              ? theme.palette.primary.main
+              : "transparent",
             padding: "8px 0",
             textTransform: "none",
             width: "100%",
@@ -118,6 +183,48 @@ const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) =>
     );
   };
 
+  useEffect(() => {
+    const defaultAdTypeValue: any = storageService.get("local", "adType");
+  
+    const findDefaultAdType = (adTypeConfig: any[], value: string) => {
+      for (const adType of adTypeConfig) {
+        // Check at the parent level
+        if (adType.value === value) {
+          return { label: adType?.title , icon: adType?.icon, value: adType?.value};
+        }
+        // Check in the nested options array
+        if (adType.options?.length) {
+          const matchedOption = adType.options.find(
+            (option: any) => option.value === value
+          );
+          if (matchedOption) {
+            return matchedOption;
+          }
+        }
+      }
+      return null; // Return null if no match is found
+    };
+  
+    // Fallback to Cars data if no match is found
+    const fallbackAdType = {
+      label: t("adtype.vehicles.Cars"),
+      icon: <DirectionsCarIcon />,
+      value: "cars",
+    };
+  
+    //const defaultAdType = defaultAdTypeValue ? findDefaultAdType(adTypeConfig, defaultAdTypeValue) || fallbackAdType : fallbackAdType;
+    const defaultAdType =
+    (defaultAdTypeValue && findDefaultAdType(adTypeConfig, defaultAdTypeValue)) ||
+    fallbackAdType;
+
+    setSelectedAdType(defaultAdType);
+    dispatch({
+      type: SET_SELECTED_ADTYPE,
+      selectedAdType: defaultAdType,
+    });
+  }, []);
+  
+
   return (
     <div>
       <Accordion
@@ -131,10 +238,14 @@ const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) =>
         onChange={handleAccordionChange("adType")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{ color: "black", display: "flex", alignItems: "center" }}>
+          <Typography
+            sx={{ color: "black", display: "flex", alignItems: "center" }}
+          >
             {selectedAdType?.icon}
             <span style={{ marginLeft: selectedAdType?.icon ? 8 : 0 }}>
-              {selectedAdType?.label ? selectedAdType.label && t("sideBar.adType") : t("sideBar.adType")}
+              {selectedAdType?.label
+                ? selectedAdType.label && t("sideBar.adType")
+                : t("sideBar.adType")}
             </span>
           </Typography>
         </AccordionSummary>
@@ -148,8 +259,18 @@ const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) =>
               sx={{ backgroundColor: theme.palette.secondary.main }}
             >
               <AccordionSummary
-               onClick={() => !options?.length ? handleButtonClick(title, icon, value) : null}
-                expandIcon={options.length ? <ExpandMoreIcon sx={{ color: theme.palette.text.primary }} /> : null}
+                onClick={() =>
+                  !options?.length
+                    ? handleButtonClick(title, icon, value)
+                    : null
+                }
+                expandIcon={
+                  options.length ? (
+                    <ExpandMoreIcon
+                      sx={{ color: theme.palette.text.primary }}
+                    />
+                  ) : null
+                }
               >
                 <Typography
                   variant="body1"
@@ -166,7 +287,9 @@ const AdType: React.FC<AdTypeProps> = ({ onOpenSwitchModal, onSelectAdType }) =>
               </AccordionSummary>
               {options.length > 0 && (
                 <AccordionDetails>
-                  {options.map(({ label, icon, value }) => renderButton(label, icon, value))}
+                  {options.map(({ label, icon, value }) =>
+                    renderButton(label, icon, value)
+                  )}
                 </AccordionDetails>
               )}
             </Accordion>
