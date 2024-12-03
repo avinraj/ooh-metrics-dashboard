4c50ceac97b52a02a7cf58a076ecdc595b4265be
avinraj <avinraj@nickelytics.com>
2024-12-03 10:16:53 +0530

    feat: Added map in attribute section

--- src/app/Attribution/components/FootfallReport.tsx
+++ src/app/Attribution/components/FootfallReport.tsx
@@ -2,10 +2,12 @@ import { Box } from "@mui/material";
 import React from "react";
 import AttributionAnalytics from "./AttributionAnalytics";
 import AttributionInsights from "./AttributionInsights";
+import MapAttribute from "./MapAttribute";
 
 const FootfallReport: React.FC = () => {
   return (
-    <Box>
+    <Box sx={{marginTop: '10px'}}>
+      <MapAttribute />
       <AttributionAnalytics />
       <AttributionInsights />
     </Box>
